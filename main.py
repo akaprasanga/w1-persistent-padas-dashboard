0dde1a587ab039a16a3b14215cf47ba762cbe9c6
akaprasanga <akaprasanganeupane@gmail.com>
2022-12-22 21:19:24 +0000

    adding files 1

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ st.write('Explanation of the entire dataset followed by Columns description.' )
 # Data Exploration
 
 my_dataset = pd.read_csv("./Cleaned_Food_Production-1.csv")
-st.write(df.head(10))
+st.write(my_dataset.head(10))
 
 livestock = ["Beef (beef herd)", "Beef (dairy herd)", "Lamb & Mutton", "Pig Meat", "Poultry Meat", "Fish (farmed)", "Shrimps (farmed)"]
 grains = ["Wheat & Rye (Bread)", "Maize (Meal)", "Oatmeal", "Rice"]
@@ -52,41 +52,6 @@ category_dataset = category.sum()
 st.plotly_chart(px.imshow(category_dataset))
 ##### edited here
 
-st.header('Exploring Dataset')
-df_corr = df.corr() # Generate correlation matrix
-x = list(df_corr.columns)
-y = list(df_corr.index)
-z = np.array(df_corr)
-
-fig = ff.create_annotated_heatmap(
-    z,
-    x = x,
-    y = y ,
-    annotation_text = np.around(z, decimals=2),
-    hoverinfo='z',
-    colorscale='Brwnyl',
-    showscale=True,
-    )
-fig.update_xaxes(side="bottom")
-fig.update_layout(
-    # title_text='Heatmap', 
-    title_x=0.5, 
-    width=500, 
-    height=500,
-    yaxis_autorange='reversed',
-    template='plotly_white'
-)
-st.write(' ')
-st.write('Correlation Heatmap')
-st.plotly_chart(fig)
-
-fig = px.histogram(df, x="salary_in_usd", color='job_title', title = 'Salaries of Varying Job Titles',
-                   color_discrete_map = {0:'#B99C6B',1:'#404F24'})
-st.plotly_chart(fig)
-
-fig = px.histogram(df, x="remote_ratio", title = 'Salaries of Occupation Location Type and Abundance',
-                  color_discrete_map = {0:'#B99C6B',1:'#404F24'})
-st.plotly_chart(fig)
 
 st.header('Drawing Conclusions')
 st.write('Based on the exploration performed, we can conclude a few things. We can conclude that the salaries for Data Science and Data Analysis positions have a truly broad range, from $40k all the way to $600k with the majority hovering around $100k. We can conclude that fully remote positions generally earn more and are more popular within the industry, nearly twice as popular as both in-office and hybrid positions. An addition thing worth noting is that the majority of instances in this dataset are from the US, and the minority being from nearly every other continent.')
