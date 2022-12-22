e356c1c89f58c46740216d52af6ae9c2404389d7
akaprasanga <akaprasanganeupane@gmail.com>
2022-12-22 21:59:46 +0000

    adding files 2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import plotly.express as px
-import warnings 
+import warnings
 import streamlit as st
 import plotly.figure_factory as ff
 warnings.filterwarnings("ignore") 
@@ -49,10 +49,70 @@ st.write(my_dataset.head(10))
 category = my_dataset.groupby('food_category')
 category_dataset = category.sum()
 
-st.plotly_chart(px.imshow(category_dataset))
 ##### edited here
+# fiona
+st.header("Correlation Plot Between Food Categories and Columns")
+st.write("In the correlation graph, it can be seen that most CO2 is emitted in the category of livestock. This includes beef herding, dairy herding, etc. In contrast, the fruit or veggie category and grains category yielded the smallest number of CO2 emissions. The dairy and oils category had the second fewest number of CO2 emissions and the /"other/" category was the category which had the second most CO2 emissions. Additionally, it is notable that the farm column released the most emissions when being compared to other columns. In conclusion, the farming and livestock release the most CO2 emissions and therefore, they are the most harmful for the planet.")
+st.plotly_chart(px.imshow(category_dataset))
 
+st.header("Histogram Chart of Land Use Changes")
+fig1 = px.histogram(my_dataset, x="Land use change")
+#fig1.show()
+st.plotly_chart(fig1)
+
+st.header("Histogram Chart of Total Emissions")
+fig2 = px.histogram(my_dataset, x="Total_emissions")
+#fig2.show()
+st.plotly_chart(fig2)
+
+st.write("While there isn't a distinct relationship between the total emissions of food production and land use changes, it can still be seen in the two histograms that when the land use changes were around 0 (meaning that human activities don't really effect the land), that was also when the total emissions are the least.")
+
+##############fiona End line
+
+######################Edward start here
+
+st.header('Food Product and Processing')
+st.write('The relationship between food product and processing')
+
+fig = px.bar(my_dataset, x='Food product', y='Processing')
+fig.update_layout(barmode='stack', xaxis={'categoryorder':'total descending'})
+#fig.show()
+st.plotly_chart(fig)
+
+st.header('Food Product and Transportation')
+st.write('The relationship between food product and the transportation cost')
+fig = px.bar(my_dataset, x='Food product', y='Transport')
+fig.update_layout(barmode='stack', xaxis={'categoryorder':'total descending'})
+#fig.show()
+st.plotly_chart(fig)
+
+#######################Edwards ending line
+
+
+######################Michael start here
+st.header('Livestock vs. Animal Feed')
+
+#st.plotly_chart(px.imshow(px.bar(my_dataset, values = 'Food product', 'Animal Feed')))
+fig = px.bar(data_livestock, x = 'Food product', y = 'Animal Feed')
+fig.update_layout(barmode='stack', xaxis={'categoryorder':'total descending'})
+st.plotly_chart(fig)
+st.write('Based on the bar graph, milk requires the least amount of animal feed in order to be produced. From the bar graph, we see that the proteins and other products such as cheese, milk, and eggs from farm animals requires a lot of animal feed to be produced')
+
+st.header('The effect of food processing on carbon emissions')
+st.plotly_chart(px.imshow(px.scatter(my_dataset, x = "Total_emissions", y = "Food product")))
+st.write('Based on the scatter plot, food products that require more processing emit more carbon into the atmosphere. This can be due to the stages of processing that occur before each product is sold.')
+
+
+#######################Michael ending line
+
+######################Nikol start here
+st.header('The Correlation Between Land Use Change and Total CO2 Emissions')
+st.write('The first pie graph represents the total CO2 emissions of each food category. According to the pie graph above, we can see that livestock accounts for the highest amount of total CO2 emissions, with over 50%. This means that they use the highest amount of most of the category of emissions. Comparing the graph to the original data, it can be seen that the category with more farming is typically what has the highest total emissions output.')
+st.plotly_chart(px.imshow(px.pie(my_dataset, values='Total_emissions', names='food_category')))
+
+st.write('The pie graph shown below represents the land use change for each food category. Similarly to the total CO2 emissions graph, here you can see that livestock has a significantly higher amount of land use change, with just over 41%. The order of highest to lowest land use change is the exact same as the order of highest to lowest CO2 emissions, showing a clear correlation between the two.')
+st.plotly_Chart(px.imshow(px.pie(my_dataset, values='Land use change', names='food_category')))
 
 st.header('Drawing Conclusions')
-st.write('Based on the exploration performed, we can conclude a few things. We can conclude that the salaries for Data Science and Data Analysis positions have a truly broad range, from $40k all the way to $600k with the majority hovering around $100k. We can conclude that fully remote positions generally earn more and are more popular within the industry, nearly twice as popular as both in-office and hybrid positions. An addition thing worth noting is that the majority of instances in this dataset are from the US, and the minority being from nearly every other continent.')
+st.write('.....')
 st.write('')
